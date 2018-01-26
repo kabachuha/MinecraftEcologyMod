fa105ac8167ddf921fce11eedcb533fb90aa329c
pupnewfster <richard@freimer.com>
2018-01-26 13:44:56 -0500

    Only show pollution values when sneaking.

--- src/main/java/ecomod/core/stuff/compat/EMWailaHandler.java
+++ src/main/java/ecomod/core/stuff/compat/EMWailaHandler.java
@@ -16,6 +16,7 @@ import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.translation.I18n;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EMWailaHandler implements IWailaDataProvider
@@ -28,11 +29,11 @@ public class EMWailaHandler implements IWailaDataProvider
 
 		if(tile != null)
 		{
+			boolean isCrouching = accessor.getPlayer().isSneaking();
 			PollutionData data = null;
 			TEPollutionConfig.TEPollution pollution = EcologyMod.instance.tepc.getTEP(tile);
-			if (pollution != null) {
+			if (pollution != null)
 				data = pollution.getEmission();
-			}
 			if(tile instanceof TileEnergy)
 				currentTip.add("Energy: " + TextFormatting.YELLOW + ((TileEnergy)tile).getEnergyStored() + " / " + ((TileEnergy)tile).getMaxEnergyStored() + " RF");
 			if(tile instanceof TileAdvancedFilter)
@@ -44,21 +45,28 @@ public class EMWailaHandler implements IWailaDataProvider
 					data = filter.getSource();
 			}
 			if (data != null) {
+				ArrayList<String> pollutionText = new ArrayList<>();
 				float air = data.getAirPollution(), soil = data.getSoilPollution(), water = data.getWaterPollution();
 				if(air <= -0.1D)
-					currentTip.add(TextFormatting.YELLOW + "" + TextFormatting.ITALIC + "Air Pollution: " + TextFormatting.GREEN + Float.toString(air));
+					pollutionText.add(TextFormatting.YELLOW + "" + TextFormatting.ITALIC + "Air Pollution: " + TextFormatting.GREEN + Float.toString(air));
 				else if(air >= 0.1D)
-					currentTip.add(TextFormatting.YELLOW + "" + TextFormatting.ITALIC + "Air Pollution: " + TextFormatting.RED + '+' + Float.toString(air));
+					pollutionText.add(TextFormatting.YELLOW + "" + TextFormatting.ITALIC + "Air Pollution: " + TextFormatting.RED + '+' + Float.toString(air));
 
 				if(water <= -0.1D)
-					currentTip.add(TextFormatting.AQUA + "" + TextFormatting.ITALIC + "Water Pollution: " + TextFormatting.GREEN + Float.toString(water));
+					pollutionText.add(TextFormatting.AQUA + "" + TextFormatting.ITALIC + "Water Pollution: " + TextFormatting.GREEN + Float.toString(water));
 				else if(water >= 0.1D)
-					currentTip.add(TextFormatting.AQUA + "" + TextFormatting.ITALIC + "Water Pollution: " + TextFormatting.RED + '+' + Float.toString(water));
+					pollutionText.add(TextFormatting.AQUA + "" + TextFormatting.ITALIC + "Water Pollution: " + TextFormatting.RED + '+' + Float.toString(water));
 
 				if(soil <= -0.1D)
-					currentTip.add(TextFormatting.GOLD + "" + TextFormatting.ITALIC + "Soil Pollution: " + TextFormatting.GREEN + Float.toString(soil));
+					pollutionText.add(TextFormatting.GOLD + "" + TextFormatting.ITALIC + "Soil Pollution: " + TextFormatting.GREEN + Float.toString(soil));
 				else if(soil >= 0.1D)
-					currentTip.add(TextFormatting.GOLD + "" + TextFormatting.ITALIC + "Soil Pollution: " + TextFormatting.RED + '+' + Float.toString(soil));
+					pollutionText.add(TextFormatting.GOLD + "" + TextFormatting.ITALIC + "Soil Pollution: " + TextFormatting.RED + '+' + Float.toString(soil));
+				if (!pollutionText.isEmpty()) {
+					if (isCrouching)
+						currentTip.addAll(pollutionText);
+					else
+						currentTip.add("Sneak to view detailed pollution information.");
+				}
 			}
 		}
 
